273e424dcd7e57249fc052e52b4bd2195997191b
KKTRKKT <paulhana6006@gmail.com>
2023-07-28 15:33:37 +0900

    converter, formatter package-info 추가

---
+++ src/main/java/me/kktrkkt/demobootweb/converter/package-info.java
@@ -0,0 +1,9 @@
+package me.kktrkkt.demobootweb.converter;
+
+/*
+Converter는 객체를 다른 객체로 변환해주는 클래스
+
+Converter<S, T> 인터페이스를 구현해 빈으로 등록해서 사용
+S타입을 T타입으로 변환함
+
+ */
---
+++ src/main/java/me/kktrkkt/demobootweb/formatter/package-info.java
@@ -0,0 +1,8 @@
+package me.kktrkkt.demobootweb.formatter;
+
+/*
+Formatter는 문자열을 객체로, 객체를 문자열로 변환해주는 클래스
+
+Formatter<T> 인터페이스를 구현해 빈으로 등록해 사용
+문자열을 T타입으로, T타입을 문자열로 변환하는 함수 두개를 구현한다
+ */
