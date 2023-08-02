c44dabdf7b19fc20f3fdfd2a5969cae803b81bf5
KKTRKKT <paulhana6006@gmail.com>
2023-08-02 23:10:27 +0900

    Create package-info.java

---
+++ src/main/java/me/kktrkkt/demobootweb/uri_pattern/package-info.java
@@ -0,0 +1,11 @@
+package me.kktrkkt.demobootweb.uri_pattern;
+
+/*
+
+URI(Uniform Resource Identifier) 리소스 고유 식별자라는 의미로, 웹상에 존재하는 웹페이지, 영상, 파일 등을 식별할 수 있는 문자열이다
+URL은 URI 하위 개념으로 리소스를 나타내는 주소를 의미한다. e.g https://www.example.com
+따라서 URL은 URI라 불러도 무방하다
+
+
+
+ */
