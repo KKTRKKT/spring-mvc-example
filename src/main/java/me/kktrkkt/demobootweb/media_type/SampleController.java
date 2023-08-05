cf790cce68a394e507059f19e6596bdd1303f89b
KKTRKKT <paulhana6006@gmail.com>
2023-08-05 17:15:06 +0900

    Update SampleController.java

--- src/main/java/me/kktrkkt/demobootweb/media_type/SampleController.java
+++ src/main/java/me/kktrkkt/demobootweb/media_type/SampleController.java
@@ -15,7 +15,10 @@ public class SampleController {
     // produces를 이용해 응답 본문의 미디어타입을 지정할 수 있다.
     @GetMapping(value = "/sample",
             consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {
+                MediaType.APPLICATION_JSON_VALUE,
+//                MediaType.APPLICATION_XML_VALUE // produces가 한개일때는 요청 헤더에 accept가 없어도 되지만 2개 이상이면 요청 헤더에 반드시 추가해야한다.
+            })
     public String getSample(@RequestBody String json){
         return json;
     }
