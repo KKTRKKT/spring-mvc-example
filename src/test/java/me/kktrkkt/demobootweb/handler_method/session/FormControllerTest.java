cc3cab4cf487b8e93786e072f02be9f3d9fc8151
r2soft <paulhana6006@gmail.com>
2023-08-23 18:00:36 +0900

    멀티폼 서브밋 테스트코드 작성 추가

--- src/test/java/me/kktrkkt/demobootweb/handler_method/session/FormControllerTest.java
+++ src/test/java/me/kktrkkt/demobootweb/handler_method/session/FormControllerTest.java
@@ -1,31 +1,37 @@
 package me.kktrkkt.demobootweb.handler_method.session;
 
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FormControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    static private MockHttpSession session;
+
+    @BeforeAll
+    static void beforeAll() {
+        session = new MockHttpSession();
+    }
+
     @Test
     @Order(1)
     void getForm1() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/form1"))
+        this.mockMvc.perform(get("/form1").session(session))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Create New Form1")))
                 .andDo(print());
@@ -34,26 +40,44 @@ class FormControllerTest {
     @Test
     @Order(2)
     void postForm1() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/form1")
-                        .param("name", "event"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Create New Form2")))
+        this.mockMvc.perform(post("/form1")
+                        .param("name", "event")
+                        .session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/form2"))
                 .andDo(print());
     }
 
     @Test
     @Order(3)
-    void getForm2() {
+    void getForm2() throws Exception {
+        this.mockMvc.perform(get("/form2").session(session))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Create New Form2")))
+                .andExpect(request().sessionAttribute("name", "event"))
+                .andDo(print());
     }
 
     @Test
     @Order(4)
-    void postForm2() {
+    void postForm2() throws Exception {
+        this.mockMvc.perform(post("/form2")
+                        .param("limit", "10")
+                        .session(session))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/last"))
+                .andExpect(request().sessionAttribute("name", "event"))
+                .andExpect(request().sessionAttribute("limit", 10))
+                .andDo(print());
     }
 
     @Test
     @Order(5)
-    void getLast() {
-
+    void getLast() throws Exception {
+        this.mockMvc.perform(get("/last").session(session))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("last")))
+                .andExpect(model().attributeExists("event"))
+                .andDo(print());
     }
 }
