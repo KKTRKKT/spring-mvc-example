c611adb619500e11879ba9b3d5741725804bd039
r2soft <paulhana6006@gmail.com>
2023-08-31 18:01:43 +0900

    requet_parsing 임시저장

---
+++ src/main/java/me/kktrkkt/demobootweb/handler_method/request_parsing/SampleController.java
@@ -0,0 +1,28 @@
+package me.kktrkkt.demobootweb.handler_method.request_parsing;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// Spring에서 본문의 contentType을 참고해 처리할 수 있는 HttpMessageConvert 선택해 변환한다.
+// SpringBoot에서 json을 object로 변환해주는 jackson 관련 객체를 자동으로 빈으로 등록해준다. JacksonAutoConfiguration 참고
+// 요청 본문을 변환 받는 방법에는 @RequestBody와 HttpEntity 등이 있다
+@RestController("/api/sample")
+public class SampleController {
+
+    @PostMapping(params = "requestBody")
+    public Sample createByRb(@RequestBody Sample sample) {
+        // save sample
+        sample.setId(1L);
+        return sample;
+    }
+
+    @PostMapping(params = "httpEntity")
+    public Sample createByHe(HttpEntity<Sample> httpEntity) {
+        // save sample
+        Sample body = httpEntity.getBody();
+        body.setId(1L);
+        return body;
+    }
+}
---
+++ src/test/java/me/kktrkkt/demobootweb/handler_method/request_parsing/SampleControllerTest.java
@@ -0,0 +1,13 @@
+package me.kktrkkt.demobootweb.handler_method.request_parsing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class SampleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+}
