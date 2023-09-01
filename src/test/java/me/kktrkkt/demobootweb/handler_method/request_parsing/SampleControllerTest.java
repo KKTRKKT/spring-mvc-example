9d2df06eb78913ba93202ae1efd4750e5ffb1217
r2soft <paulhana6006@gmail.com>
2023-09-01 15:47:56 +0900

    request_parsing 예제 추가

--- src/test/java/me/kktrkkt/demobootweb/handler_method/request_parsing/SampleControllerTest.java
+++ src/test/java/me/kktrkkt/demobootweb/handler_method/request_parsing/SampleControllerTest.java
@@ -1,13 +1,56 @@
 package me.kktrkkt.demobootweb.handler_method.request_parsing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class SampleControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createByRb() throws Exception {
+        Sample sample = new Sample();
+        sample.setName("sample");
+
+        this.mockMvc.perform(post("/api/sample?requestBody")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sample)))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("createByRb"))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("sample"))
+                .andDo(print());
+    }
+
+    @Test
+    void createByHe() throws Exception {
+        Sample sample = new Sample();
+        sample.setName("sample");
+
+        this.mockMvc.perform(post("/api/sample?httpEntity")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sample)))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("createByHe"))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("sample"))
+                .andDo(print());
+    }
 }
